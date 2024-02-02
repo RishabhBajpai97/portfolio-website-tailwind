d2b972f27466f5553ee281a7e8007bcf22d5778b
RishabhBajpai97 <bajpai.rishabh1018@gmail.com>
2024-02-02 17:38:39 +0530

    update resume file path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,146 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaTwitter, FaMedium } from "react-icons/fa"
-import { HiOutlineMail } from "react-icons/hi"
-import { BsPersonLinesFill } from "react-icons/bs"
-import { Link } from "react-scroll"
-import Logo from "../assets/logo.png"
-import Resume from "../assets/Resume/RishabhBajpai.pdf"
+import React, { useState } from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaMedium,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
+import Logo from "../assets/logo.png";
+const reumePath = process.env.PUBLIC_URL + "/assets/Resume/RishabhBajpai.pdf";
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "80px" }} />
+      </div>
+      <div className="hidden md:flex">
+        <ul className="flex">
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="md:hidden z-10" onClick={handleClick}>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* Social Icons */}
+      <div className="hidden lg:flex flex-col fixed top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 ">
+            <a
+              className="flex justify-between items-center text-gray-300 w-full"
+              href="https://www.linkedin.com/in/rishabh-bajpai-b87454152"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] ">
+            <a
+              className="flex justify-between items-center text-gray-300 w-full"
+              href="https://github.com/RishabhBajpai97"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] ">
+            <a
+              className="flex justify-between items-center text-gray-300 w-full"
+              href="mailto:bajpai.rishabh1018@gmail.com"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] ">
+            <a
+              className="flex justify-between items-center text-gray-300 w-full"
+              href="https://medium.com/@bajpai.rishabh1018"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Medium <FaMedium size={30} />
+            </a>
+          </li>
 
-    return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-            <div>
-                <img src={Logo} alt="Logo Image" style={{ width: "80px" }} />
-            </div>
-            <div className='hidden md:flex'>
-                <ul className='flex'>
-                    <li><Link to='home' smooth={true} duration={500}>Home</Link></li>
-                    <li><Link to='about' smooth={true} duration={500}>About</Link></li>
-                    <li><Link to='skills' smooth={true} duration={500}>Skills</Link></li>
-                    <li><Link to='work' smooth={true} duration={500}>Work</Link></li>
-                    <li><Link to='contact' smooth={true} duration={500}>Contact</Link></li>
-                </ul>
-            </div>
-            <div className='md:hidden z-10' onClick={handleClick}>
-                {!nav ? <FaBars /> : <FaTimes />}
-            </div>
-            {/* Mobile Menu */}
-            <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-                <li className='py-6 text-4xl' ><Link onClick={handleClick} to='home' smooth={true} duration={500}>Home</Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to='about' smooth={true} duration={500}>About</Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to='skills' smooth={true} duration={500}>Skills</Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to='work' smooth={true} duration={500}>Work</Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to='contact' smooth={true} duration={500}>Contact</Link></li>
-            </ul>
-            {/* Social Icons */}
-            <div className='hidden lg:flex flex-col fixed top-[35%] left-0'>
-                <ul>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 '>
-                        <a className='flex justify-between items-center text-gray-300 w-full' href="https://www.linkedin.com/in/rishabh-bajpai-b87454152" target='_blank' rel="noreferrer">LinkedIn <FaLinkedin size={30} /></a>
-                    </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] '>
-                        <a className='flex justify-between items-center text-gray-300 w-full' href="https://github.com/RishabhBajpai97" target='_blank' rel='noreferrer'>Github <FaGithub size={30} /></a>
-                    </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] '>
-                        <a className='flex justify-between items-center text-gray-300 w-full' href="mailto:bajpai.rishabh1018@gmail.com">Email <HiOutlineMail size={30} /></a>
-                    </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] '>
-                        <a className='flex justify-between items-center text-gray-300 w-full' href="https://medium.com/@bajpai.rishabh1018" target='_blank' rel='noreferrer'>Medium <FaMedium size={30} /></a>
-                    </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#8892b0] ">
+            <a
+              className="flex justify-between items-center text-gray-300 w-full"
+              href={reumePath}
+              download="RishabhBajpai.pdf"
+            >
+              Resume <BsPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#8892b0] '>
-                        <a className='flex justify-between items-center text-gray-300 w-full' href={Resume} download="RishabhBajpai.pdf">Resume <BsPersonLinesFill size={30} /></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
